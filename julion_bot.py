2c714e29c0cf9862c554b4d7845d4b6a7640f3b6
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2021-05-16 21:53:02 +0530

    [*] Null commit message

---
+++ julion_bot.py
@@ -0,0 +1,212 @@
+from discord.ext.commands.errors import CommandInvokeError, CommandNotFound
+import libjulion, requests, os, discord, json
+from discord.ext import commands, tasks
+from bs4 import BeautifulSoup as scraper
+from more_itertools import sliced
+
+bot_name='Julion - The Poet'
+
+info = {}
+REFRESH_TIME=300
+
+def embed_generator(data, t):
+    separator=['. ', '\r'][t]
+    title, poetry, poet = data
+    subembeds = []
+    try:
+        sliced_data=list(sliced(poetry, 980))
+        embed = discord.Embed(title=title, color=0x71368a)
+        embed.add_field(name=poet, value=sliced_data[0], inline=False)
+        sliced_data.pop(0)
+        left_over='-'
+        for i in sliced_data:
+            sembed=discord.Embed(title='continued...', color=0x71368a)
+            s=i.split(separator)[-1]
+            i=i.replace(s, '')
+            sembed.add_field(name='-', value=left_over+i, inline=False)
+            subembeds.append(sembed)
+            left_over='-'
+            left_over+=s
+        return embed, subembeds
+    except IndexError:pass
+
+def enisable_text_channel(channel_id, status, lang_code):
+    global info
+    info[str(channel_id)][lang_code]=status
+    with open('info.json', 'w') as filename:
+        json.dump(info, filename)
+
+def help_embed():
+    embed = discord.Embed(title=format(bot_name), color=0x71368a)
+    embed.add_field(
+        name="Description:", value="This bot is designed for posting Hindi and English poems automatically on discord.", inline=False)
+    embed.add_field(
+        name="**Commands:**\n", value="`register` : Command used for registering this channel.\n`deregister` : Command used for deregistering this channel.\n`enable [hindi/english]` : Command used for enabling language of poem in this channel. \n `disable [hindi/english]` : Command used for disabling language of poem in this channel.", inline=False)
+    embed.add_field(
+        name="Invite: ", value="You can get invite link by typing `invite`")
+    embed.add_field(
+        name="Source: ", value="You can get source code by typing `source`")
+    embed.add_field(
+        name="Credits: ", value="You can get credits info by typing `credits`")
+    return embed
+
+def invite_embed():
+    embed = discord.Embed(title='{} Invite'.format(bot_name),url='https://discord.com/api/oauth2/authorize?client_id=843312879318466612&permissions=321600&scope=bot',
+                description='Invite {} on your server.'.format(bot_name), color=0x71368a)
+    return embed
+
+def source_embed():
+    source_code = 'https://github.com/0x0is1/APOD-bot'
+    embed = discord.Embed(title='{} Source code'.format(bot_name),
+                          url=source_code,
+                          description='Get {} Source Code.'.format(bot_name), color=0x71368a)
+    return embed
+
+bot = commands.Bot(command_prefix='%')
+bot.remove_command('help')
+
+@bot.command(name='help')
+async def help(ctx):
+    embed = help_embed()
+    await ctx.send(embed=embed)
+
+@tasks.loop(seconds=REFRESH_TIME)
+async def main_fun():
+    global info
+    en_url=requests.get(url=libjulion.random_url_generator(0))
+    en_soup=scraper(en_url.content, 'html.parser')
+    en_data=libjulion.english_poetry_generator(en_soup)
+    hi_url=requests.get(url=libjulion.random_url_generator(1))
+    hi_soup=scraper(hi_url.content, 'html.parser')
+    hi_data=libjulion.hindi_poetry_generator(hi_soup)
+    channel_ids = list(info.keys())
+    for channel_id in channel_ids:
+        en_status=info[channel_id][0]
+        hi_status=info[channel_id][1]
+        if en_status=='ON':
+            embed=embed_generator(en_data, 0)
+            channel_ob = bot.get_channel(int(channel_id))
+            try:
+                await channel_ob.send(embed=embed[0])
+                for i in embed[1]:await channel_ob.send(embed=i)
+            except TypeError:pass
+        if hi_status=='ON':
+            embed=embed_generator(hi_data, 1)
+            channel_ob = bot.get_channel(int(channel_id))
+            try:
+                await channel_ob.send(embed=embed[0])
+                for i in embed[1]:await channel_ob.send(embed=i)
+            except TypeError:pass
+
+@bot.event
+async def on_ready():
+    global info
+    info = json.load(open('info.json', 'r'))
+    print('Bot status: Online.')
+    main_fun.start()
+
+@bot.command()
+async def source(ctx):
+    embed = source_embed()
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong! `{}ms`'.format(round(bot.latency * 1000)))
+
+@bot.command()
+async def credits(ctx):
+    embed = discord.Embed(title=format(bot_name), color=0x71368a)
+    embed.add_field(name='Disclaimer', value='This API is Owned by `Kavyashala` and `poetry.com`.\nWe do not claim any site related property.\nWe do not promote any illegal use of this API/Bot.', inline=False)
+    embed.add_field(name='Developer', value='0x0is1', inline=False)
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def status(ctx):
+    global info
+    channel_id = ctx.message.channel.id
+    channels = info
+    channel_ids = list(channels.keys())
+    if str(channel_id) in channel_ids:
+        embed = discord.Embed(color=0x71368a)
+        s=channels[str(channel_id)]
+        embed.add_field(name='Status:', value='English: {0}\nHindi: {1}'.format(s[0], s[1]), inline=False)
+        await ctx.send(embed=embed)
+
+@bot.command()
+async def enable(ctx, lang='english'):
+    lang_code={'english': 0, 'hindi': 1}[lang]
+    channel_id = ctx.message.channel.id
+    enisable_text_channel(channel_id, 'ON', lang_code)
+    await ctx.message.add_reaction('✅')
+
+@bot.command()
+async def disable(ctx, lang='english'):
+    lang_code={'english': 0, 'hindi': 1}[lang]
+    channel_id = ctx.message.channel.id
+    enisable_text_channel(channel_id, 'OFF', lang_code)
+    await ctx.message.add_reaction('✅')
+
+@bot.command()
+async def deregister(ctx):
+    channel_id = ctx.message.channel.id
+    channels = json.load(open('info.json', 'r'))
+    channel_ids = list(channels.keys())
+    if str(channel_id) in channel_ids:
+        global info
+        info.pop(str(channel_id))
+        with open('info.json', 'w') as filename:
+            json.dump(info, filename)
+        embed = discord.Embed(color=0x71368a)
+        embed.add_field(name='Info: ',
+                        value='This channel is no more subscribed for R.O.C.E \n Use `enable` or `disable` commands to resubscribe.', inline=False)
+        await ctx.send(embed=embed)
+
+@bot.command()
+async def register(ctx):
+    channel_id = ctx.message.channel.id
+    channels = json.load(open('info.json', 'r'))
+    channel_ids = list(channels.keys())
+    if not str(channel_id) in channel_ids:
+        global info
+        info[str(channel_id)] = []
+        info[str(channel_id)].append('OFF')
+        info[str(channel_id)].append('OFF')
+        with open('info.json', 'w') as filename:
+            json.dump(info, filename)
+        embed = discord.Embed(color=0x71368a)
+        embed.add_field(name='Info: ', value='This channel is no more subscribed now. \n Use `enable` or `disable` commands to unsubscribe.', inline=False)
+        await ctx.send(embed=embed)
+    else:
+        await ctx.send(message='This channel is already registered.')
+
+@disable.error
+async def disable_error(ctx, error):
+    await ctx.send('`Channel is not registered. Please use register command to register.`')
+    print(error)
+
+@enable.error
+async def enable_error(ctx, error):
+    await ctx.send('`Channel is not registered. Please use register command to register.`')
+    print(error)
+
+@register.error
+async def register_error(ctx, error):
+    await ctx.send('`Channel might be already registered.`')
+    print(error)
+
+@deregister.error
+async def deregister_error(ctx, error):
+    await ctx.send('`Channel might not be already registered.`')
+    print(error)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        await ctx.send('`Unknown command` \n Please use right command to operate. `help` for commands details.')
+    if isinstance(error, CommandInvokeError):
+        return
+    print(error)
+
+auth_token = os.environ.get('EXPERIMENTAL_BOT_TOKEN')
+bot.run(auth_token)
---
+++ libjulion.py
@@ -0,0 +1,85 @@
+import requests, random
+from bs4 import BeautifulSoup as scraper
+EP_DOWNLIMIT = 50100
+EP_UPLIMIT = 58885
+
+def english_poetry_generator(soup):
+    poetry = ''
+    heading = soup.find(
+        'h1', {'class': 'c-hdgSans c-hdgSans_2 c-mix-hdgSans_inline'})
+    if heading is None: heading = scraper('N/A', 'html.parser')
+    poe = soup.find('span', {'class': 'c-txt c-txt_attribution'})
+    if poe is None: poe = scraper('N/A', 'html.parser')
+    data = soup.find('div', {'data-view': 'PoemView'})
+    if data is None: data = scraper('N/A', 'html.parser')
+    lines = data.findAll(
+         'div', {'style': 'text-indent: -1em; padding-left: 1em;'})
+    if lines is None: lines = scraper('N/A', 'html.parser')
+    header = ' '.join(heading.text.split())
+    poet = ' '.join(poe.text.split())
+
+    for line in lines:
+        poetry += (' '.join(line.text.split()) + '\n')
+    return header, poetry, poet
+
+
+def random_url_generator(t):
+    cookies = {
+        '__cfduid': 'db0d58dc7534df31c08ea6f14311e80cc1606313489',
+        'XSRF-TOKEN': 'eyJpdiI6IjhaU0cxNWNwSWRUaGZKdGFQd1VZMVE9PSIsInZhbHVlIjoiQzhmTnE2RzBVVlEyaEp5SjVaQVdtRHA3cTc3YUk3ejBQcE5XUGs4RGR2MGRhbzlRQ2RUMGtuS1R3UmNrUjl1VSIsIm1hYyI6IjhkYzI1YTEyM2NlYWM0MmMyYTZiMzllODg0M2Q1YzQ5MmUwMDExYTY1YzA0MzcyOWIxZjQ3MTQyNzlhZWI0MzkifQ%3D%3D',
+        'laravel_session': 'eyJpdiI6IllIeTI1VW9OOW9ndnRyaW9CenpCckE9PSIsInZhbHVlIjoiTDFDK1ZlZTMxWlwvTnhDbzNUQXh2R3hvNm9CZ0lnTG5SYWJKVUV2d2V2eVQxcXc5aGI5YTRaMllDRno0MEdUMnEiLCJtYWMiOiIzOTkyMDgxZGYzNmZjZjE2YzJhZmYyNmQ1ODE3ZGE5MDRkMDQyYjNmMjEzNjlkN2UzZDk0YzViZTBiOGNjNzdkIn0%3D',
+    }
+
+    headers = {
+        'Host': 'kaavyaalaya.org',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv: 75.0) Gecko/20100101 Firefox/75.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'DNT': '1',
+        'Connection': 'close',
+        'Upgrade-Insecure-Requests': '1',
+    }
+    if t==0:
+        rp = random.randint(EP_DOWNLIMIT, EP_UPLIMIT)
+        url = "https://www.poetryfoundation.org/poems/" + str(rp)
+        return url
+    if t==1:
+        response = requests.head(
+            'https://kaavyaalaya.org/s/random', headers=headers, cookies=cookies)
+        return str(response.headers.get('location'))
+        
+
+def hindi_poetry_generator(soup):
+    # This Python file uses the following encoding: utf-8
+    heading = soup.find('div', {
+        'style': 'margin-bottom:20px;text-align: center;font-weight: bold;text-decoration: underline;font-size: 22px;'})
+    if heading is None: heading = scraper('N/A', 'html.parser')
+    header = ' '.join(heading.text.split())
+    poetry = soup.find('div', {
+        'style': 'display: table;margin:0 auto;font-size: 20px;font-family: Mangal;max-width:98%'})
+    poe = soup.find(
+        'div', {'style': 'text-align: right;font-weight: bold;margin-top: 20px'})
+    if poetry is None: poetry = scraper('N/A', 'html.parser')
+    if poe is None: poe = scraper('N/A', 'html.parser')
+    poet = ' '.join(poe.text.split())
+    poetr = ''
+    i = 0
+    while True:
+        try:
+            line = poetry.findAll('br')[i].next_sibling
+            if line != '          ':
+                poetr += line
+            i += 1
+        except Exception:
+            break
+    if len(poetr)==0: poetr[0]='N/A'
+    return header, poetr, poet
+
+    
+'''url = random_url_generator(1)
+response = requests.get(url)
+soup = scraper(response.content.decode('utf-8'), 'html.parser')
+a=str(hindi_poetry_generator(soup)[1]).split('\n')
+print('\r' in a)
+print(a)'''
